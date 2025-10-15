dc6bb2d06344ba6b352c57e29316e844d9343323
liamspence <wrspence@uvm.edu>
2025-10-15 11:49:08 -0400

    Downloaded piece pngs from the internet

--- Piece.py
+++ Piece.py
@@ -1,8 +1,10 @@
 import arcade
 
-class Piece:
-    def __init__(self, color, start_position):
+class Piece(arcade.Sprite):
+    def __init__(self, image_file, center_x, center_y, color, start_position, piece_type):
+        super().__init__(image_file, center_x=center_x, center_y=center_y)
         self.color = color
+        self.piece_type = piece_type
         self.start_position, self.curr_position = start_position
         self.possible_moves = []
 
@@ -16,3 +18,5 @@ class Piece:
     # Move function to be overriden by subclasses
     def move(self):
         pass
+
+white_pawn = Piece('white_pawn.png', 0, 0)
